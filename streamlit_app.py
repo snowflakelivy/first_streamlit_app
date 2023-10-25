4b4f19cb52aa595dc9bcdb87652d60aa45806e62
snowflakelivy <148838146+snowflakelivy@users.noreply.github.com>
2023-10-25 11:34:59 +1100

    Update streamlit_app.py
    added dataframe and kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+#take the json version of response and make it plain English
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#show the df
+streamlit.dataframe(fruityvice_normalized)
